8c64009ae591ba4a1307d9f29448b0248525b370
Aaron Haughton <dreamcast2000@outlook.com>
2020-03-17 23:09:51 -0400

    initial commit

---
+++ HungryDevs/HungryDevs/Model.swift
@@ -0,0 +1,43 @@
+//
+//  Model.swift
+//  HungryDevs
+//
+//  Created by Joe on 3/17/20.
+//  Copyright © 2020 AlphaGradeINC. All rights reserved.
+//
+
+import Foundation
+
+
+class Spoon {
+    
+    func pickUp() {
+        
+    }
+    
+    func putDown() {
+        
+    }
+    
+}
+
+
+class Developer {
+    
+    var leftSpoon: Bool = true
+    var rightSpoon: Bool = false
+    
+    func think() {
+        
+    }
+    
+    func eat() {
+        
+    }
+    
+    func run() {
+    
+    }
+    
+    
+}
